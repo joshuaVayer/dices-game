e7077f1506251866a42f053c07a2045e65985c9f
Joshua Vayer <77019747+joshuaVayer@users.noreply.github.com>
2021-07-20 13:27:38 +0200

    Call dice on same eventListener

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ btnVideo.addEventListener('click', () => {
  video.style.visibility = 'visible';
 /* La video est lancée */
   video.play();
+/* Get the face value*/
+ dice();
 });
 /* A la fin de la video, on peut cacher la video avec */
 // video.onended = () => {
@@ -15,7 +17,6 @@ btnVideo.addEventListener('click', () => {
 // }
 
 
-const roll = document.getElementById('btn-roll');
 let faces = ["videos/1.mp4", "videos/2.mp4", "videos/3.mp4", "videos/4.mp4","videos/5.mp4","videos/6.mp4"];
 
 // fonction donnant un chiffre aléatoire entre 1 et 6 et associé au dé
@@ -23,9 +24,7 @@ const dice = () => {
   let valueDice = Math.floor(Math.random() *  faces.length);
   let pointsDice = faces.indexOf(faces[number]) + 1;
 // affichage des videos correspondantes
-}
-
-roll.addEventListener('click', dice);
+};
   
 
 
